0238eff5a04b60e7f696749fdaf1068996c96bab
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-04 00:26:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -17,5 +17,7 @@ end
 def all_phrases
 puts "It's-a me, Mario!"
 end
-puts ""
+puts "Thank You Mario! But Our Princess Is In Another Castle!"
+end
+puts "It's Dangerous To Go Alone! Take This."
 end
