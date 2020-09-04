fb00405097d4a42ff6914bc6d126161cd1d6a588
Matthew Kindt <matthewkindt@yahoo.com>
2020-09-04 00:09:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -15,8 +15,8 @@ def link(phrase = "It's Dangerous To Go Alone! Take This.")
 end
 
 def all_phrases(phrase2, phrase3)
-  phrase3 = 
-  
+  phrase3 = "It's Dangerous To Go Alone! Take This."
+  phrase2 = 
   phrase = "It's-a me, Mario!"
   puts phrase3
   puts phrase2
